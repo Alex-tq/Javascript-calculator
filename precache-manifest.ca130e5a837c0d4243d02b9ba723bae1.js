cf0d0c9eb4dc6ec71b773c2246a6dda5b6f60639
unknown <alextq3036@gmail.com>
2021-10-20 01:29:09 -0400

    Updates

---
+++ precache-manifest.ca130e5a837c0d4243d02b9ba723bae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a39398e12857041cf44e85602d1a4a",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "6fc076bf5c71a1e18067",
+    "url": "/javascript-calculator/static/css/main.8909e6ee.chunk.css"
+  },
+  {
+    "revision": "c635bb295dcff1503d3c",
+    "url": "/javascript-calculator/static/js/2.3f20c472.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/javascript-calculator/static/js/2.3f20c472.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fc076bf5c71a1e18067",
+    "url": "/javascript-calculator/static/js/main.7c59c0b8.chunk.js"
+  },
+  {
+    "revision": "830328696600e3933e42",
+    "url": "/javascript-calculator/static/js/runtime-main.85d63bc8.js"
+  }
+]);
